9a5e2161cdf30931a34e802da9760e65d2cd443e
Ridwan1821 <Kolawoleridwan416@gmail.com>
2024-05-22 00:02:43 +0100

    python script updated

--- classifier_app.py
+++ classifier_app.py
@@ -11,6 +11,7 @@ import pickle
 import re
 import nltk
 nltk.download('stopwords')
+import stopwords
 #from nltk.corpus import stopwords
 stop = stopwords.words('english')
 from nltk.stem import PorterStemmer
